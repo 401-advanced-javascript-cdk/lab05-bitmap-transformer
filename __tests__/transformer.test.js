45e9e3018120f3d2b83ef5b4bbb8029232c371d3
Chris Kozlowski <kozlowskicd@gmail.com>
2019-05-18 18:47:46 -0700

    added test framework;  read -> alter -> write cycle complete, ready to begin on creating transformations and modularization.

---
+++ __tests__/transformer.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const transformer = require('../lib/transformer.js');
+
+// process.argv.push('./assets/baldy.bmp');
+// process.argv.push('greyscale');
+
+let args = ['node', 'jest', './assets/baldy.bmp', 'greyscale'];
+
+describe('transformer', () => {
+  it('fails without two arguments', () => {
+    expect( () => transformer()).toThrow();
+  });
+  it('takes 2 arguments on the command line', () => {
+    expect( () => transformer(args.slice(2)) ).not.toThrow();
+  });
+});
